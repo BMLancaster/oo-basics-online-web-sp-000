9b95e643cfb5d073d43b5fbe857097ecafb35414
Benjamin M Lancaster <bmlancaster07@gmail.com>
2019-01-02 20:09:33 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -12,16 +12,3 @@ class Book
   
 end
 
-class Shoe
-  attr_accessor :color, :size,:material,:condition
-  attr_reader :brand
-  
-  def initialize(brand)
-    @brand = brand
-  end
-  
-  def cobble
-    puts "The shoe has been repaired"
-  end
-  
-end
